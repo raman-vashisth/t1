c3fd9e51b1fb1c7ba607d295af59b0e2389beebc
RAMAN VASHISTH <70128332+raman-vashisth@users.noreply.github.com>
2024-02-09 17:12:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function login() {
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
-	if (username == "admin" && password == "admin") {
+	if (username == "raman" && password == "admin") {
 		alert("Login successful!");
 		window.location.href = "Sequential_ticket_Counter.html";
 	} else {
